3c594e4b4b6955c4f393faedcca8399b798f1c48
Ryan Zhiyang Tan <ryatan@c2r6s14.42singapore.sg>
2026-04-21 19:14:47 +0800

    update

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 11:49:52 by ryatan            #+#    #+#             */
-/*   Updated: 2026/04/21 13:55:54 by ryatan           ###   ########.fr       */
+/*   Updated: 2026/04/21 17:27:56 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@
 # include "mlx.h"
 # include "mlx_int.h"
 # include "../get_next_line/get_next_line.h"
+
+typedef struct	s_win_data
+{
+	void	*mlx;
+	void	*win;
+}	t_win_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include "fdf.h"
 
+int	close_handler(t_win_data *w_data)
+{
+	mlx_destroy_window(w_data->mlx, w_data->win);
+	return (0);
+}
+
 int	main(void)
 {
-	int	fd;
-	//const char *path = "/home/ryatan/core/fdf_home/test_maps/42.fdf";
-	const char *path = "/home/ryantz/dev/42/fdf/test_maps/42.fdf";
-	char	*line;
-	t_xvar *mlx_ptr;
+	int			fd;
+	const char *path = "/home/ryatan/core/fdf_home/test_maps/42.fdf";
+	//const char *path = "/home/ryantz/dev/42/fdf/test_maps/42.fdf";
+	char		*line;
+	t_win_data w_data;
 
 	fd = open(path, O_RDONLY);
 	ft_printf("%d", fd);
@@ -18,9 +24,13 @@ int	main(void)
 			ft_printf("%s", line);
 			free(line);
 		}
+		else
+			break;
 	}
-	mlx_ptr = mlx_init();
-	mlx_new_window(mlx_ptr, 100, 100, "new window");
-	mlx_loop(mlx_ptr);
+	close(fd);
+	w_data.mlx = mlx_init();
+	w_data.win = mlx_new_window(w_data.mlx, 700, 700, "new window");
+	mlx_hook(w_data.win, 17, 0, close_handler, &w_data);
+	mlx_loop(w_data.mlx);
 	return (0);
 }
