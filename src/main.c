cf8517eeaafa2d3e815e6e75c0917522ca22ca49
ryantz <ryantanzhi1997@gmail.com>
2026-04-21 16:15:50 +0800

    update

--- src/main.c
+++ src/main.c
@@ -3,15 +3,22 @@
 int	main(void)
 {
 	int	fd;
-	const char *path = "/home/ryatan/core/fdf_home/test_maps/42.fdf";
+	//const char *path = "/home/ryatan/core/fdf_home/test_maps/42.fdf";
+	const char *path = "/home/ryantz/dev/42/fdf/test_maps/42.fdf";
 	char	*line;
 	t_xvar *mlx_ptr;
 
 	fd = open(path, O_RDONLY);
 	ft_printf("%d", fd);
-	line = get_next_line(fd);
-	ft_printf("%s", line);
-
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line != NULL)
+		{
+			ft_printf("%s", line);
+			free(line);
+		}
+	}
 	mlx_ptr = mlx_init();
 	mlx_new_window(mlx_ptr, 100, 100, "new window");
 	mlx_loop(mlx_ptr);
